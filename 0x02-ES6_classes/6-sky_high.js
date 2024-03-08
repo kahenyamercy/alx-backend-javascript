1ef8dedbac1be5d43383f9d2ca91b8f7dbdac764
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-08 04:17:17 +0300

    modified file to remove duplicates line of code

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -18,23 +18,3 @@ export default class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this.floors} floors`;
   }
 }
-import Building from './5-building';
-
-export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this.floors = floors;
-  }
-
-  get floors() {
-    return this._floors;
-  }
-
-  set floors(value) {
-    this._floors = value;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
-  }
-}
