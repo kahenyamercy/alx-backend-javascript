d166042e6ba130830b4614f8f8ce7f68847d197f
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-12 00:08:15 +0300

    reduce function

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+// Function that returns the sum of all the student ids.
+
+export default function getStudentIdsSum(students) {
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
+}
