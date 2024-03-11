9511d37ce71d7786b73a103b96c64f4ea733f53b
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-12 00:20:16 +0300

    combined filter and map function to update student grades by cities

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,19 @@
-// Function that returns array of students for specific city with their new grade
+// Description: Update the grade of the student by city.
 
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((student) => student.location === city).map((student) => ({
-    ...student,
-    grade: newGrades.find((grade) => grade.studentId === student.id).grade,
-  }));
+  const defaultGrade = { grade: 'N/A' };
+
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
