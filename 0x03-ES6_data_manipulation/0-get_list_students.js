37172ce922f7053e6287d0d92d79b8de36f672ec
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-11 13:57:56 +0300

    a function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// Function that returns an array of objects
+// Each object contains three attributes
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
