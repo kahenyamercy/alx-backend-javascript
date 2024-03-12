1ac6731eb32f2ff797f5aade0b098cbc5b23f29e
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-12 21:58:12 +0300

    function that checks if all elements in the array exist within the set

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,5 @@
+// Function that returns a boolean if all the elements in the array exist within the set.
+
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
