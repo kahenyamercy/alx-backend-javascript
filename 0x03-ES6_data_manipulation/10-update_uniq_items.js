3bd5bc834305f9631380afab9fc7d7dbcd8c5d17
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-13 00:16:18 +0300

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,17 @@
+export default function updateUniqueItems(inputMap) {
+  if (!(inputMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = new Map();
+
+  for (const [key, value] of inputMap.entries()) {
+    if (value === 1) {
+      updatedMap.set(key, 100);
+    } else {
+      updatedMap.set(key, value);
+    }
+  }
+
+  return updatedMap;
+}
