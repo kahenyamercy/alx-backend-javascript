91450a2c63633d0db45c917d328b0bf5e0468805
kahenyamercy <kahenyamercy5@gmail.com>
2024-05-02 13:53:08 +0300

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable jest/require-hook */
+/* eslint-disable indent */
+const assert = require('assert');
+const { it, describe } = require('mocha');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+    it('checking if numbers round', () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('checking if numbers round 2nd round', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('checking if numbers round 3nd round', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('checking if numbers round 4nd round', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+    it('checking negative return', () => {
+        assert.equal(calculateNumber(-1.3, -3.7), -5);
+      });
+});
